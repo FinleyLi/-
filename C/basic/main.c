df1b2c49964ccecfc05414348710d9bd26070b68
Finley <galoislee@yahoo.com.tw>
2019-10-21 09:12:23 +0800

    Add files via upload

---
+++ C/basic/main.c
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+1060304APCSconcept_03
+
+Thanks, GDB online is an online compiler and debugger tool...
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+// Return value
+int Finley(){
+    // code goes here
+    return 9527;
+}
+
+// no Return value
+void FinleyLi(){
+    // code goes here
+}
+
+int main(void)
+{
+    printf( "Hello World" );
+    printf( "\n%d", Finley() );
+}

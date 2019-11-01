a89f25ab78113c478dcd38ebcec66b753d1e4af7
Finley <galoislee@yahoo.com.tw>
2019-11-01 16:34:23 +0800

    Update First Java Program.java

--- JAVA/cooc李帆老師基礎程式設計Java/章節二、經典名句Hello World/First Java Program.java
+++ JAVA/cooc李帆老師基礎程式設計Java/章節二、經典名句Hello World/First Java Program.java
@@ -4,7 +4,21 @@ public class MyFirstJavaProgram {
     * This will print 'Hello World' as the output
     */
 
-   public static void main(String []args) {
-      System.out.println("Hello World"); // prints Hello World
-   }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*if (args.length == 0){
+            System.out.println("無引數..");
+        }
+		
+		else {*/		
+		System.out.println("顯示命令列引數..");
+        /*for(String arg : args) {
+        System.out.println(arg);
+        }
+        
+        System.out.println( "args[0]" + args[0]);  
+        System.out.println( "args[1]" + args[1]);
+		}*/
+
+	}
 }

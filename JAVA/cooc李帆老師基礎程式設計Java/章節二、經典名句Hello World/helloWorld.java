ae824e2a2ad91e9dbcd8b9ae1c5a01157bc9758e
Finley <galoislee@yahoo.com.tw>
2019-11-02 01:13:53 +0800

    Update helloWorld.java

--- JAVA/cooc李帆老師基礎程式設計Java/章節二、經典名句Hello World/helloWorld.java
+++ JAVA/cooc李帆老師基礎程式設計Java/章節二、經典名句Hello World/helloWorld.java
@@ -8,20 +8,47 @@ package coocJava;
  * Author: Finley@ms.tsh.ttu.edu.tw
  * */
 
-public class helloWorld {
-	// 建構式 Constructor
-	/*public helloWorld(){
-		System.out.print("hello, world");
-	}*/
+class Hello{
+    private String name;
+
+    public Hello(){
+        name = "nobody";
+    }
+
+    public Hello(String one){
+        name = one;
+    }
+
+    public void hello(){
+        System.out.print("Hello ");
+        System.out.println(name);
+    }
+
+    public void setName(String one){
+        name = one;
+    }
+}
+
+public class Main
+{
+
 	// static變數就是在載入程式後會主動配給記憶體給程式(僅一次)，後續無論實例化多少次，記憶體位置都一樣。
 	private static void say(String s) {
-		System.out.print("hello, world");
+		System.out.println(s);
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		say("hello, world");
-
+        
+        // 指定引數建立 hello 實例
+        Hello hello = new Hello("Java");
+        // 呼叫 hello()方法
+        hello.hello();
+ 
+        // 呼叫 setName()改變 name 的參考對象
+        hello.setName("caterpillar");
+        hello.hello();
 	}
 
 }

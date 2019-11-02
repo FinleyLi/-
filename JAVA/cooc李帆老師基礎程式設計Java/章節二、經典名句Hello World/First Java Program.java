2532fb11f8657ebd508e53d97465dbf5af92f16f
Finley <galoislee@yahoo.com.tw>
2019-11-02 11:10:19 +0800

    Update First Java Program.java

--- JAVA/cooc李帆老師基礎程式設計Java/章節二、經典名句Hello World/First Java Program.java
+++ JAVA/cooc李帆老師基礎程式設計Java/章節二、經典名句Hello World/First Java Program.java
@@ -4,6 +4,10 @@ public class MyFirstJavaProgram {
     * This will print 'Hello World' as the output
     */
 
+	// public宣告主函式為public就是告訴其他的類可以訪問這個函式。
+	// static告知編譯器main函式是一個靜態函式，記憶體的儲存區不變。
+	// void表明main()的返回值是無型別。
+	// 引數String[] args為程式使用者在命令列狀態下與程式互動。
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		if (args.length == 0) {

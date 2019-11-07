c9c65df5206bc39e43ba646d22bf90c5574c105b
Finley <galoislee@yahoo.com.tw>
2019-11-07 23:26:43 +0800

    Update Creating an Object.java

--- JAVA/cooc李帆老師基礎程式設計Java/章節三、Object and Classes/Creating an Object.java
+++ JAVA/cooc李帆老師基礎程式設計Java/章節三、Object and Classes/Creating an Object.java
@@ -1,26 +1,30 @@
-package coocJava;
-
-/*
- * 章節三 Object and Class
- * Problem: Creating an Object Puppy
- * Input: Null
- * Output: Passed Name is tommy
- * Author: Finley@ms.tsh.ttu.edu.tw
- * */
+class sayC{
+     public sayC(){
+        System.out.print("Hello, ??");
+    }
+}
 
 public class CreatingAnObject {
 	
-	public CreatingAnObject(String name) {
+	public  CreatingAnObject(String name) {
+	//public static void CreatingAnObject(String name) {
 	      // This constructor has one parameter, name.
-	      System.out.println("Passed Name is :" + name );
+	      System.out.println("Passed Name is: " + name );
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// Following statement would create an object myPuppy
-		CreatingAnObject myPuppy = new CreatingAnObject( "tommy" );
+		/* Following statement would create an object myPuppy */
+		CreatingAnObject myPuppy = new CreatingAnObject( "Tommy" );
+		
+		/* Call Method CreatingAnObject */
+		//CreatingAnObject( "Tommy" );
+		
 		/* Object creation */
-		// Puppy myPuppy = new Puppy( "tommy" );
+		// Puppy myPuppy = new Puppy( "Tommy" );
+		
+		// 指定引數建立(實體化) hello C實例
+		//sayC helloC = new sayC();
 
 	}
 

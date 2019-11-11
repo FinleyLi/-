97a03a0275463753cc64dc1aad1acb94f6edb480
Finley <galoislee@yahoo.com.tw>
2019-11-11 13:27:10 +0800

    Update Creating an Object.java

--- JAVA/cooc李帆老師基礎程式設計Java/章節三、Object and Classes/Creating an Object.java
+++ JAVA/cooc李帆老師基礎程式設計Java/章節三、Object and Classes/Creating an Object.java
@@ -20,15 +20,15 @@ public class CreatingAnObject { //public 修飾符
 	
 	public static void main(String[] args) { //void 返回類型保留字
 		// TODO Auto-generated method stub
+		System.out.print("hello World");
+		
 		/* Following statement would create an object myPuppy */
-		CreatingAnObject myPuppy = new CreatingAnObject( "Tommy" );
+		//CreatingAnObject myPuppy = new CreatingAnObject( "Tommy" );
 		
 		/* Call Method CreatingAnObject */
-		sayA( "Tommy" );
+		//sayA( "Tommy" );
 		
 		/* Object creation */
-		// Puppy myPuppy = new Puppy( "Tommy" );
-		
 		// 指定引數建立(實體化) hello C實例
 		//sayHello World = new sayHello();
 

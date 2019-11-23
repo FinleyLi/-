e20758f98ea2e63dc525491bbd970aa14bef1c76
Finley <galoislee@yahoo.com.tw>
2019-11-23 14:45:21 +0800

    Update Elephant.java

--- JAVA/cooc李帆老師基礎程式設計Java/章節三、Object and Classes/Elephant.java
+++ JAVA/cooc李帆老師基礎程式設計Java/章節三、Object and Classes/Elephant.java
@@ -1,9 +1,10 @@
 // package coocJava;
 // default package
+// 如果你在宣告類別時沒有加上任何存取修飾， 則預設為package存取範圍，該類別僅限於同一個package下的成員所使用。
 
 public class Elephant extends Animal{
 	
-	// 如果你在宣告類別時沒有加上任何存取修飾， 則預設為package存取範圍，該類別僅限於同一個package下的成員所使用。
+	
 	String name="";
 	
 	void speak() {
